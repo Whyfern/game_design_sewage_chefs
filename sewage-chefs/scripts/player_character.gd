8a260929d1a38c85633e3c669842e34ff15c9810
Whyfern <tijn@nulens.nl>
2026-09-17 15:03:53 +0200

    added a player character that you can move using wasd. as well as a toilet sprite

---
+++ sewage-chefs/scripts/player_character.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var speed = 120
+var motion = Vector2()
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
+func get_input():
+	motion = Vector2.ZERO
+	if Input.is_action_pressed("press_a"):
+		motion.x = -speed
+		print(motion)
+	if Input.is_action_pressed("press_d"):
+		motion.x = speed
+		print(motion)
+	if Input.is_action_pressed("press_w"):
+		motion.y = -speed
+		print(motion)
+	if Input.is_action_pressed("press_s"):
+		motion.y = speed
+		print(motion)
+	velocity = motion
