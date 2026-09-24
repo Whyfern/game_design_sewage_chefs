eaef94fd4835ff14707a816b88058dd56a0c013f
Whyfern <tijn@nulens.nl>
2026-09-24 15:24:08 +0200

    moved some files and you can now use arrow keys to move character

--- sewage-chefs/scripts/level_manager.gd
+++ sewage-chefs/scripts/level_manager.gd
@@ -1,13 +1,15 @@
 extends Node
 
 @onready var restaurant = load('res://scenes/restaurant.tscn')
+@onready var sewer = load('res://scenes/sewer.tscn')
 @onready var main = get_node("..")
 
 var restaurantInstance : Node = null
+var sewerInstance : Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#instatiate_restaurant()
+	instatiate_restaurant()
 	pass
 
 
@@ -20,14 +22,20 @@ func instatiate_restaurant():
 	restaurantInstance.z_index = 0
 	main.add_child.call_deferred(restaurantInstance)
 	
+
+func instantiate_level():
+	sewerInstance = sewer.instantiate()
+	sewerInstance.z_index = 0
+	main.add_child.call_deferred(sewerInstance)
+	pass
+
 	#var restraunt = 1
-		#
+	#
 	#if instance and is_instance_valid(instance):
 		#instance.queue_free()
 		#instance = null
-	#
+	#+
 	#instance_count += 1
 	#instance = creation_panel.instantiate()
 	#instance.name = "creationpanel%d" % instance_count
 	#UI.add_child.call_deferred(instance)
-	##print(instance.name)
