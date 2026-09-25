c7019de050a1c94e6ccceb24b673f9b1452cf632
Daria Shulepov <d.shulepov@student.tue.nl>
2026-09-25 14:26:08 +0200

    Made the player stay on the screen

--- sewage-chefs/scripts/player_character.gd
+++ sewage-chefs/scripts/player_character.gd
@@ -3,14 +3,19 @@ extends CharacterBody2D
 @export var speed = 120 * 3
 var motion = Vector2()
 
+#var viewRect := get_viewport_rect()
+
 func _ready() -> void:
 	pass
+	
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
 
 func get_input():
+	position.x = clamp(position.x, $CollisionShape2D.shape.size.x / 2, get_viewport_rect().size.x - $CollisionShape2D.shape.size.x / 2)
+	position.y = clamp(position.y, $CollisionShape2D.shape.size.x / 2, get_viewport_rect().size.y - $CollisionShape2D.shape.size.x / 2)
 	motion = Vector2.ZERO
 	if Input.is_action_pressed("press_a"):
 		motion.x = -speed
